ff54f521fd6d521c7e81c8e2dc17970953bd6166
tekocher-su <tekocher@syr.edu>
2022-12-15 22:20:41 -0500

    drtfgyvb

---
+++ project/project7maybe/storepages/slideshow.js
@@ -0,0 +1,4 @@
+function slideshow(){getElementById('bigimg1').style.display = 'none'; 
+getElementById('bigimg2').style.display = 'none'}
+function imgon(){document.getElementById('bigimg1').style.display = 'block'}
+function imgon(){document.getElementById('bigimg2').style.display = 'block'}
